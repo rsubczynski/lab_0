5626c9b43c2a6b64f731f5f5f54e35dca5ea22a7
unknown <student@L404-7.carbon.cti.p.lodz.pl>
2017-11-25 17:02:53 +0100

    zakonieczenie zdania

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -3,11 +3,15 @@ package edu.iis.mto.lab_0;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TailsGenerator {
+class TailsGenerator {
 
-	public List<String> tails(String value) {
-		List<String> tails = new ArrayList<>(value.length());		
-		return tails;
-	}
+    List<String> tails(String value) {
+        List<String> tails = new ArrayList<>(value.length());
+        for (int i = 0; i < value.length() + 1; i++) {
+            tails.add(value.substring(i));
+        }
+        System.out.println(tails);
+        return tails;
+    }
 
 }
--- src/test/java/edu/iis/mto/lab_0/TailsTest.java
+++ src/test/java/edu/iis/mto/lab_0/TailsTest.java
@@ -9,31 +9,39 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TailsTest {
-	
-	private static final String HELLO = "Hello";
-	private TailsGenerator tailsGenerator;
-	private List<String> tails;
-	@Before
-	public void setup(){
-		
-		tailsGenerator = new TailsGenerator();
-		tails = tailsGenerator.tails(HELLO);
-	}
 
-	@Test
-	public void tailsShouldReturnListOfStringsOfSizeEqualsToInputStringLengthPlusOne() {
-		
-		assertThat(tails, Matchers.hasSize(HELLO.length() + 1));
-	}
-	@Test
-	public void tailsShouldReturnFullStringAsFirstElement() {
-		
-		assertThat(tails.get(0), Matchers.equalTo(HELLO));
-	}
-	@Test
-	public void tailsShouldReturnEmptyStringAsLastElement() {
-		
-		assertThat(tails.get(tails.size()-1), Matchers.equalTo(""));
-	}
+    private static final String HELLO = "Hello";
+    private static final String HELLO_THIRTY_ELEMENT = "lo";
+    private List<String> tails;
+
+    @Before
+    public void setup() {
+
+        TailsGenerator tailsGenerator = new TailsGenerator();
+        tails = tailsGenerator.tails(HELLO);
+    }
+
+    @Test
+    public void tailsShouldReturnListOfStringsOfSizeEqualsToInputStringLengthPlusOne() {
+
+        assertThat(tails, Matchers.hasSize(HELLO.length() + 1));
+    }
+
+    @Test
+    public void tailsShouldReturnFullStringAsFirstElement() {
+
+        assertThat(tails.get(0), Matchers.equalTo(HELLO));
+    }
+
+    @Test
+    public void tailsShouldReturnEmptyStringAsLastElement() {
+
+        assertThat(tails.get(tails.size() - 1), Matchers.equalTo(""));
+    }
+
+    @Test
+    public void tailsShouldReturnThirtyStringAsThirtyElement() {
+        assertThat(tails.get(3), Matchers.equalTo(HELLO_THIRTY_ELEMENT));
+    }
 
 }
